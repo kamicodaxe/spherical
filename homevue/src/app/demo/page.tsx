3a4119fd2f004d1dc1d5ec05c472962074ee9bfe
Loïc Kami <lkami@bogial.com>
2023-10-10 20:50:52 +0200

    Moved Home page to Demo Page

---
+++ homevue/src/app/demo/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Viewer from "@/components/Viewer";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen">
+      <Viewer />
+    </main>
+  )
+}
