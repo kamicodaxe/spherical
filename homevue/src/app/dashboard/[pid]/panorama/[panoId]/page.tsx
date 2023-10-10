c21580af37515166783353d455a0b74a3d7425a2
Loïc Kami <lkami@bogial.com>
2023-10-11 00:14:17 +0200

    Panorama page using Sphericad component

--- homevue/src/app/dashboard/[pid]/panorama/[panoId]/page.tsx
+++ homevue/src/app/dashboard/[pid]/panorama/[panoId]/page.tsx
@@ -1,4 +1,8 @@
-
+import connect from "@/lib/connect";
+import { Project, User } from "@/lib/models";
+import { IUser } from "@/lib/models/User";
+import { Shericad } from "@/components/shericad";
+import { IProject } from "@/lib/models/Project";
 
 interface PanoramaProps {
 	params: {
@@ -6,16 +10,34 @@ interface PanoramaProps {
 		panoId: string
 	}
 }
-export default function Panorama({
+{/* The magic of Spericad3D is in this component */ }
+export default async function Panorama({
 	params: {
 		pid,
 		panoId
 	}
 }: PanoramaProps) {
-	// Show the panorama in the viewer with all props
+
+	await connect()
+	const user = await User.findOne({ email: "lkami@bogital.com" })
+	// @ts-ignore Pass only data needed to client components
+	const userObj: IUser = {
+		id: user.id.toString(),
+		firstName: user.firstName,
+		lastName: user.lastName,
+	}
+	const project = await Project.findOne({ ownerId: user.id, _id: pid })
+	// @ts-ignore Pass only data needed to client components
+	const projectObj: IProject = {
+		ownerId: project.ownerId.toString(),
+		id: project.id.toString(),
+		name: project.name,
+		description: project.description,
+	}
+
 	return (
-		<main className="w-full min-h-screen">
-			<p>Show Panorama</p>
+		<main className="flex flex-col">
+			<Shericad user={userObj as IUser} project={projectObj} panoId={panoId} />
 		</main>
 	)
 }
